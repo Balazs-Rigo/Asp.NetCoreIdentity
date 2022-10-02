4a71cdeded68cba2a3cd44e9251e1b50d063694f
Balazs Rigo <rigo.balazs86@gmail.com>
2022-10-02 17:38:39 +0200

    End Of Section 2

--- IdentityManager_Own/IdentityManager_Own/Data/ApplicationDbContext.cs
+++ IdentityManager_Own/IdentityManager_Own/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using IdentityManager_Own.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace IdentityManager_Own.Data
@@ -9,5 +10,7 @@ namespace IdentityManager_Own.Data
         {
 
         }
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
     }
 }
--- IdentityManager_Own/IdentityManager_Own/Startup.cs
+++ IdentityManager_Own/IdentityManager_Own/Startup.cs
@@ -2,6 +2,7 @@ using IdentityManager_Own.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,6 +28,7 @@ namespace IdentityManager_Own
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddControllersWithViews();
         }
@@ -48,7 +50,7 @@ namespace IdentityManager_Own
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
